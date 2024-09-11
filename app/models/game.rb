ea6653bad51c7c7108587dceb63e28157db27ba9
Daniel Muller Moutinho <dan.muller141@gmail.com>
2024-09-11 17:17:48 -0300

    home + botões

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,7 +2,7 @@ class Game < ApplicationRecord
   belongs_to :user
   has_many :players, dependent: :destroy
   has_many :game_messages, dependent: :destroy
-  #has_one_attached :photo
+  has_one_attached :photo
 
   validates :name, :pg, :system, presence: :true
 
